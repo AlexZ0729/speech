9c26eb719f332cf5245c1cade6e2284db15c8c49
Ankita Pasad <ankitap@ttic.edu>
2020-05-11 20:06:40 -0500

    added path for activating torch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Run `source setup.sh` from this directory.
+
+. ~/torch/install/bin/torch-activate
+
 export PYTHONPATH=`pwd`:`pwd`/libs/warp-ctc/pytorch_binding:`pwd`/libs:$PYTHONPATH
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libs/warp-ctc/build
