22b91764697e8a12e3c38c4d3caf6a0317aebafd
Ankita Pasad <ankitap@ttic.edu>
2020-05-11 20:39:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Run `source setup.sh` from this directory.
 
-. ~/torch/install/bin/torch-activate
-
 export PYTHONPATH=`pwd`:`pwd`/libs/warp-ctc/pytorch_binding:`pwd`/libs:$PYTHONPATH
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libs/warp-ctc/build
